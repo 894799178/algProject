d2037c563580e0cbfc7677a3cc4e7c9c1eb25836
Tester <894799178@qq.com>
2018-12-07 19:28:34 +0800

    简单算法

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import string.dispon.TheLongestPalindrome;
+
+public class Main {
+
+    private static TheLongestPalindrome theLongestPalindrome = new TheLongestPalindrome();
+
+    public static void main(String[] args) {
+
+        Integer length = theLongestPalindrome.exe("abababa");
+        System.out.println("长度:"+length);
+    }
+
+
+
+}
---
+++ src/string/dispon/TheLongestPalindrome.java
@@ -0,0 +1,27 @@
+package string.dispon;
+
+/**
+ * 最长回文子串
+ */
+public class TheLongestPalindrome {
+    /**
+     *  执行最长回文算法
+     * @param str 需要一个主 字符串
+     * @return 返回最长回文子串的长度
+     */
+    public Integer exe(String str){
+        char[] chars = str.toCharArray();
+        int maxLength = 0;
+        int mid  = 0;
+        int l =0;
+        int r = chars.length-1;
+        int length = chars.length;
+        while(mid <length){
+
+        }
+        return maxLength;
+    }
+
+
+
+}
