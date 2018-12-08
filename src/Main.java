ee77197329f08e7725031df00bfe74255ab8ffcb
xiaoxianyu <894799178@qq.com>
2018-12-08 23:18:50 +0800

    最长回文算法  时间复杂度 O(n^2)

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-import string.dispon.TheLongestPalindrome;
+
+import java.util.Scanner;
 
 public class Main {
 
-    private static TheLongestPalindrome theLongestPalindrome = new TheLongestPalindrome();
+   // private static TheLongestPalindrome theLongestPalindrome = new TheLongestPalindrome();
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row = scanner.nextInt();
+        String [] str= new String[row];
+        for (int i =0; i<row;i++){
+           str[i]= scanner.next();
+        }
+        for (int i = 0;i<str.length;i++){
+            System.out.println(test(str[i]));
+        }
 
-        Integer length = theLongestPalindrome.exe("abababa");
-        System.out.println("长度:"+length);
     }
-
+    public static int test(String str){
+        if(str == null ||  "".equals(str)){
+            System.out.println(0);
+        }
+        char[] chars = str.toCharArray();
+        int maxLength = 0;
+        int mid  = 1;
+        int l,r;
+        int length = chars.length;
+        while(mid <length){
+            int tempSum = 1;
+            l = mid-1;
+            r = mid+1;
+            while (l >= 0 && r <= (length-1)){
+                if(chars[l] == chars[r]){
+                    tempSum+=2;
+                }else{
+                    break;
+                }
+                l--;
+                r++;
+            }
+            mid++;
+            if(tempSum >maxLength){
+                maxLength = tempSum;
+                if(maxLength == length){
+                    return maxLength;
+                }
+            }
+        }
+        return maxLength;
+    }
 
 
 }
--- src/string/dispon/TheLongestPalindrome.java
+++ src/string/dispon/TheLongestPalindrome.java
@@ -10,14 +10,34 @@ public class TheLongestPalindrome {
      * @return 返回最长回文子串的长度
      */
     public Integer exe(String str){
+        if(str == null ||  "".equals(str)){
+            return 0;
+        }
         char[] chars = str.toCharArray();
         int maxLength = 0;
-        int mid  = 0;
-        int l =0;
-        int r = chars.length-1;
+        int mid  = 1;
+        int l,r;
         int length = chars.length;
+        System.out.println("length:"+length);
         while(mid <length){
-
+            int tempSum = 1;
+            l = mid-1;
+            r = mid+1;
+            while (l >= 0 && r <= (length-1)){
+                if(chars[l] == chars[r]){
+                    tempSum+=2;
+                }else{
+                    break;
+                }
+                System.out.println(mid);
+                l--;
+                r++;
+            }
+            mid++;
+            if(tempSum >maxLength){
+                maxLength = tempSum;
+                System.out.println("maxLength:"+maxLength);
+            }
         }
         return maxLength;
     }
